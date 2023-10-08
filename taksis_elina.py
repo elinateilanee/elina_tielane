5ed4305eddbb885968e126d9cce190e11601501a
elinateilanee <144446906+elinateilanee@users.noreply.github.com>
2023-10-08 18:58:20 +0300

    Add files via upload

--- taksis_elina.py
+++ taksis_elina.py
@@ -1,23 +1,44 @@
-taksatarifsnakti = 0.77
-taksatarifsdiena = 0.37
-pasazieri = int(input('Pasažieru skaits taksī:'))
-if pasazieri >4 or pasazieri==0:
-    print('Piedodied tāc cilvēku skaits nevar braukt')
-elif pasazieri <4:
-    print('Jūs drīkstat braukt')
-laiks = int(input('Iveadi laiku kad vēlies izbraukt:'))
-if laiks > 21 or laiks < 6:
-    print(taksatarifsnakti)
-elif laiks:
-    print(taksatarifsdiena)
-noligsana = (input('Vai jūs redzat taksi stacijas stāvvietā? j/n:'))
-if noligsana == 'j':
-    print('Nolīgšanas maksa ir 2 eiro')
-elif noligsana == 'n':
-        print('Nolīgšanas maksa ir 4,50 eiro')
-gaidisanas_laiks =  (input('Vai jūs vajadzēs gaidīt? j/n:'))
-if gaidisanas_laiks == 'j':
-     print('Būs jāmaksā 0,15 par 1 min')
-elif gaidisanas_laiks == 'n':
-     print('Ok')
-attālums = (input('Cik tālu vēlaties braukt?:'))
+dzivnieki = input('Vai jums būs kamielis taksī? j/n: ')
+if dzivnieki == 'j':
+    print('Atvainojiet, bet šim taksistam ir alerģija no kamieļiem')
+    exit()
+elif dzivnieki == 'n':
+    pasazieri = int(input('Pasažieru skaits taksī: '))
+    if pasazieri >= 4 or pasazieri <= 1:
+        print('Piedodied tāds cilvēku skaits nevar braukt')
+        exit()
+    elif 1 <= pasazieri <= 4:
+        laiks = float(input('Ievadies laiku kad Jūs braucat: '))
+        if 6 <= laiks <= 21:
+            diena = 0.37
+        else:
+            nakts = 0.77
+
+    noligsana = input('Vai Jūs redzat taksi stacijas stāvvietā? j/n: ')
+    if noligsana == 'j':
+        noligsanas_maksa_parasta = 2.00
+    elif noligsana == 'n':
+        noligsanas_maksa_papildus = 2.00 + 2.50
+
+    gaidisanas_laiks = input('Vai Jūs vajadzēs gaidīt j/n: ')
+    if gaidisanas_laiks == 'n':
+        nav_gaidisanas_laika = 0
+    elif gaidisanas_laiks == 'j':
+        gaidisanas_maksa = 0.15
+        gaidisanas_laiks_minutes = int(input('Cik minūtes?: '))
+    attalums = float(input('Cik tālu vēlaties braukt? (km): '))
+
+    print('\n\t čeks ')
+    print(f'izsaukuma laiks: {laiks:.2f}')
+    print(f'gaidīšanas laiks: { nav_gaidisanas_laika or gaidisanas_laiks_minutes:.2f} min')
+    print(f'attālums: {attalums:.2f} km')
+    print('\n\ttarifu aprēķins')
+    print('attāluma summa:', attalums*diena or attalums*nakts)
+    print('izsaukums:', noligsanas_maksa_parasta or noligsanas_maksa_papildus)
+    print('gaidīšana:', gaidisanas_maksa*gaidisanas_laiks_minutes or nav_gaidisanas_laika)
+    kopsumma = attalums*diena or attalums*nakts + noligsanas_maksa_parasta or noligsanas_maksa_papildus + gaidisanas_maksa*gaidisanas_laiks_minutes or nav_gaidisanas_laika
+    print(f'\n\t kopsumma: {kopsumma:.2f}')
+    print()
+    print('Paldies, ka izbraucāt ar MēsMīlamKamieļus<3')
+else:
+    print('Ups ir notikusi kļūda :c')
